3f474e55f5d1531e03e9a6907b039f7f9331841a
Cristiano Pacheco <chris.spb25@gmail.com>
2018-01-04 00:10:18 -0200

    added react router

--- client-app/src/components/App.js
+++ client-app/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import AppRoutes from './app-routes'
+
 class App extends Component {
   render () {
     return (
-      <div>App</div>
+      <div>
+        <AppRoutes />
+      </div>
     )
   }
 }
